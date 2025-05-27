e302f5afefa7c8cafead498ce8c087fb6a31faee
anhlamlee <anhlam01693347568@gmail.com>
2025-05-27 19:49:45 +0700

    small change

--- src/app/_components/alert.tsx
+++ src/app/_components/alert.tsx
@@ -18,7 +18,7 @@ const Alert = ({ preview }: Props) => {
         <div className="py-2 text-center text-sm">
           {preview ? (
             <>
-              This page is a preview.{" "}
+              This Page is a preview.{" "}
               <a
                 href="/api/exit-preview"
                 className="underline hover:text-teal-300 duration-200 transition-colors"
